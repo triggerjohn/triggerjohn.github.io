0bf805a4c7ad88054a153d92608c9eced6e71d8d
John Salter <john.salter@triggerglobal.com>
2017-10-18 15:46:53 -0700

    25 percent marker

--- aframe/ar.js
+++ aframe/ar.js
@@ -88,7 +88,7 @@ var asm=(function(global,env,buffer) {
             this._initialize();
 
         }
-        artoolkit.setPattRatio(this.id, 0.5);
+        artoolkit.setPattRatio(this.id, 0.25);
         console.log(artoolkit.getPattRatio(this.id));
     };
 
